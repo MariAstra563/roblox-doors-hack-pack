c14503bd3cadd714df4466fab3eee656302a46a6
AstroCOL563 <124411319+AstroCOL563@users.noreply.github.com>
2023-03-19 03:22:26 -0400

    Roblox Rooms Raw Code

--- RoomsHackPack.lua
+++ RoomsHackPack.lua
@@ -20,7 +20,38 @@ text2.BackgroundColor3 = Color3.new(1, 1, 1)
 text2.Size = UDim2.new(0, 100, 0, 100)
 text2.Position = UDim2.new(0, 0, 0, 100)
 text2.Parent = game.Players[playerName].PlayerGui.TestGui
-while true do
+while wait() do
+    for i, o in pairs(game.Workspace:GetChildren()) do
+        if o:FindFirstChild("Humanoid") and o.Name ~= game.Players.LocalPlayer.Name then
+            if not o:FindFirstChild("HighlightA") then
+                local hl = Instance.new("Highlight")
+                hl.Name = "HighlightA"
+                hl.Parent = o
+                hl.OutlineTransparency = 1
+                hl.FillTransparency = 0.25
+                hl.FillColor = Color3.new(0, 1, 0)
+            end
+        end
+        if o:FindFirstChild("HumanoidRootPart") and o.Name ~= playerName then
+            if not o.HumanoidRootPart:FindFirstChild("DisplayUserGui") then
+                local dug = Instance.new("BillboardGui")
+                dug.Name = "DisplayUserGui"
+                dug.AlwaysOnTop = true
+                dug.Parent = o.HumanoidRootPart
+                dug.Size = UDim2.new(0, 100, 0, 100)
+                local ug = Instance.new("TextLabel")
+                ug.Parent = dug
+                ug.Size = UDim2.new(0, 100, 0, 100)
+                ug.TextScaled = true
+                ug.Text = o.Name
+                ug.BorderSizePixel = 0
+                ug.BackgroundTransparency = 1
+                ug.TextStrokeColor3 = Color3.new(0, 0, 0)
+                ug.TextStrokeTransparency = 0
+                ug.TextColor3 = Color3.new(0, 1, 0)
+            end
+        end
+    end
     if game.Workspace:FindFirstChild("A60") then
         if game.Workspace.A60:FindFirstChild("Main") then
             local a60 = game.Workspace.A60.Main.Position
@@ -71,5 +102,4 @@ while true do
         text2.Text = "No A120"
         text2.BackgroundColor3 = Color3.new(1, 1, 1)
     end
-    wait()
 end
